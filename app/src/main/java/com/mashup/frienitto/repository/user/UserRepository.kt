4be9d05e9c6cb6dad6e3fef8bda84d3806be3b3a
pengin7384 <pengin7384@gmail.com>
2019-06-30 03:40:05 +0900

    Add UserRepository

--- app/src/main/java/com/mashup/frienitto/repository/user/UserRepository.kt
+++ app/src/main/java/com/mashup/frienitto/repository/user/UserRepository.kt
@@ -1,14 +1,39 @@
 package com.mashup.frienitto.repository.user
 
 import com.mashup.frienitto.api.DataSource
-import com.mashup.frienitto.data.RequestEmailCode
-import com.mashup.frienitto.data.ResponseEmailCode
+import com.mashup.frienitto.data.*
 import io.reactivex.Single
 
 object UserRepository {
 
+    private var token: String = ""
+
+    // User
+    fun signUp(token: String, body: RequestSignUp) : Single<ResponseSignUp> {
+        return DataSource.requestSignUp(token, body)
+    }
+
+    fun signIn(body: RequestSignIn): Single<ResponseSignIn> {
+        return DataSource.requestSignIn(body)
+    }
+
+
+    // Email
+
     fun requestEmail(body: RequestEmailCode) : Single<ResponseEmailCode> {
         return DataSource.requestEmailCode(body)
+    }
+
+    fun requestAuth(body: RequestAuth) : Single<ResponseAuth> {
+        return DataSource.requestAuth(body)
+    }
+
+
+    // Token
+    fun getTokenizer() = token
+
+    fun setTokenizer(token: String) {
+        this.token = token
     }
 
 }
