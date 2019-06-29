54bffd0fc13c9c5cf41bd399b09ee8ee86ae213c
pengin7384 <pengin7384@gmail.com>
2019-06-30 03:39:05 +0900

    Add SignUp, SignIn, Auth in Service

--- app/src/main/java/com/mashup/frienitto/api/Service.kt
+++ app/src/main/java/com/mashup/frienitto/api/Service.kt
@@ -1,14 +1,32 @@
 package com.mashup.frienitto.api
 
-import com.mashup.frienitto.data.RequestEmailCode
-import com.mashup.frienitto.data.ResponseEmailCode
+import com.mashup.frienitto.data.*
 import io.reactivex.Single
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface Service {
 
+    // User
+    @POST("api/v1/sign-up")
+    fun requestSignUp(@Header("X-Register-Token") token: String,
+                      @Body body: RequestSignUp) : Single<ResponseSignUp>
+
+    @POST("api/v1/sign-in")
+    fun requestSingIn(@Body body: RequestSignIn) : Single<ResponseSignIn>
+
+
+    // Room
+
+
+    // Email
+
     @POST("api/v1/issue/code")
     fun requestEmailCode(@Body body: RequestEmailCode) : Single<ResponseEmailCode>
+
+    @POST("api/v1/verify/code")
+    fun requestAuth(@Body body: RequestAuth) : Single<ResponseAuth>
+
 
 }
